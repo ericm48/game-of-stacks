90bd941b2e367f5a917af252973b00869ab10741
Amit Singh <singhamitch@outlook.com>
2023-03-16 22:26:14 +0530

    updates setup script

--- kpack-setup.sh
+++ kpack-setup.sh
@@ -1,22 +1,20 @@
-#!/usr/bin/env bash
-set -e
-
-alias k=kubectl
+#!/bin/bash
+set -eu
 
 export KPACK_VERSION=$1
-k apply -f https://github.com/pivotal/kpack/releases/download/v$KPACK_VERSION/release-$KPACK_VERSION.yaml
+kubectl apply -f https://github.com/pivotal/kpack/releases/download/v$KPACK_VERSION/release-$KPACK_VERSION.yaml
 
 sleep 10
 
 export RUN_IMAGE=$2
-k apply -f ./kpack-resources/cluster-stack.yaml
+kubectl apply -f ./kpack-resources/cluster-stack.yaml
 
-k apply -f ./kpack-resources/cluster-store.yaml
+kubectl apply -f ./kpack-resources/cluster-store.yaml
 
-k apply -f ./kpack-resources/cluster-builder.yaml
+kubectl apply -f ./kpack-resources/cluster-builder.yaml
 
 sleep 10
 
 export APP_NAME=$3
 export APP_REPO=$4
-k apply -f ./kpack-resources/image.yaml
+kubectl apply -f ./kpack-resources/image.yaml
